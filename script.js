3e91db45de4a9a50a3956a6ddf25ebd3dd9ee9c0
Navneet_thakur <127828104+navneet451@users.noreply.github.com>
2024-10-28 10:20:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 //your JS code here. If required.
+document.querySelector("form").addEventListener("submit",(event)=>{
+	event.preventDefault();
+	const name=document.getElementById("name").value;
+	const age=document.getElementById("age").value;
+
+	if(!name || !age){
+		alert("inputs can't be empty");
+		return;
+	}
+	function ageValidation(age, name) {
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                if (parseInt(age) >= 18) {
+                    resolve(`Welcome, ${name}. You can vote.`);
+                } else {
+                    reject(`Oh sorry ${name}. You aren't old enough.`);
+                }
+            }, 4000); // 4-second delay
+        });
+    }
+	ageValidation(age,name)
+          .then((result)=>{
+			  alert(result);
+		  })
+          .catch((error)=>{
+			  alert(error);
+		  });
+});
